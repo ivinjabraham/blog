768db74e204bdf892adc1e4f9b816c1bd60b9c8e
Ivin Joel Abraham <ivinjabraham@gmail.com>
2025-02-10 21:58:33 +0530

    update consts with personal info

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,6 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
+export const SITE_URL = "https://ivinjabraham.pages.dev/";
+export const SITE_TITLE = "ivinjabraham";
 export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
+  "Code, systems and optimizations";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "ivinjabraham@gmail..com";
